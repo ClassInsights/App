37233651ab93817b8c46914b8614b1f48f3d55e9
Jakkoble <business@jakkoble.de>
2023-05-05 19:13:26 +0200

    added default structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import './screens/dashboard_screen.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
       ),
+      routes: {
+        '/': (_) => const DashboardScreen(),
+      },
     );
   }
 }
