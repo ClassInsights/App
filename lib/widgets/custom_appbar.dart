be83c037361ea5227f0977d2dbab7da66d4f45ee
Jakob <business@jakkoble.de>
2023-07-02 20:23:11 +0200

    added Custom Appbar

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,59 @@
+import "package:flutter/material.dart";
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double height;
+  final String title;
+  final int index;
+
+  const CustomAppBar(
+      {super.key,
+      required this.height,
+      required this.title,
+      required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top, bottom: 40.0),
+      padding: const EdgeInsets.only(top: 10),
+      height: height,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title),
+                index == 2
+                    ? Text(
+                        "Abmelden",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.error,
+                        ),
+                      )
+                    : Icon(
+                        Icons.add,
+                        color: Theme.of(context).colorScheme.primary,
+                      )
+              ],
+            ),
+            const SizedBox(height: 5.0),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: Divider(
+                color: Theme.of(context).colorScheme.onSurface,
+                thickness: 2.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+}
