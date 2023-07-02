82dbcbdb8af1fea0111b3c379f62d4a0d4045929
Jakob <business@jakkoble.de>
2023-07-02 20:39:13 +0200

    added screen header

---
+++ lib/widgets/header.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  final String title;
+
+  const Header({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const SizedBox(height: 20.0),
+      ],
+    );
+  }
+}
