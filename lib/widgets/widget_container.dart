fd5df13cc69929806ecfd8e7458b58da6cc34871
Jakob <business@jakkoble.de>
2023-07-03 19:40:34 +0200

    added showArrow option

--- lib/widgets/widget_container.dart
+++ lib/widgets/widget_container.dart
@@ -6,7 +6,8 @@ class WidgetContainer extends StatelessWidget {
   final String? label;
   final String title;
   final Widget? child;
-  final Function? action;
+  final Function? onTab;
+  final bool showArrow;
 
   const WidgetContainer({
     super.key,
@@ -15,7 +16,8 @@ class WidgetContainer extends StatelessWidget {
     this.label,
     required this.title,
     this.child,
-    this.action,
+    this.onTab,
+    this.showArrow = false,
   });
 
   @override
@@ -23,57 +25,86 @@ class WidgetContainer extends StatelessWidget {
     final backgroundColor = primary
         ? Theme.of(context).colorScheme.primaryContainer
         : Theme.of(context).colorScheme.secondaryContainer;
-    return Container(
-      width: width,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20.0,
-        vertical: 15.0,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8.0),
-        color: backgroundColor,
-        boxShadow: [
-          BoxShadow(
-            color: backgroundColor.withOpacity(.5),
-            blurRadius: 5.0,
-            offset: const Offset(2.0, 4.0),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          label == null
-              ? const SizedBox()
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      label.toString(),
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                    ),
-                    const SizedBox(
-                      height: 6.0,
-                    ),
-                  ],
-                ),
-          Text(
-            title,
-            style: Theme.of(context).textTheme.titleSmall,
-          ),
-          child == null
-              ? const SizedBox()
-              : Column(
-                  children: [
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    child!,
-                  ],
-                ),
-        ],
+    return GestureDetector(
+      onTap: onTab == null ? null : onTab as void Function()?,
+      child: Container(
+        width: width,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 15.0,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          color: backgroundColor,
+          boxShadow: [
+            BoxShadow(
+              color: backgroundColor.withOpacity(.5),
+              blurRadius: 5.0,
+              offset: const Offset(2.0, 4.0),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  label == null
+                      ? const SizedBox()
+                      : Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              label.toString(),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                    color: primary
+                                        ? Theme.of(context)
+                                            .colorScheme
+                                            .background
+                                        : Theme.of(context)
+                                            .colorScheme
+                                            .secondary,
+                                  ),
+                            ),
+                            const SizedBox(
+                              height: 6.0,
+                            ),
+                          ],
+                        ),
+                  Text(title,
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                            color: primary
+                                ? Theme.of(context).colorScheme.background
+                                : Theme.of(context).colorScheme.secondary,
+                          )),
+                  child == null
+                      ? const SizedBox()
+                      : Column(
+                          children: [
+                            const SizedBox(
+                              height: 20.0,
+                            ),
+                            child!,
+                          ],
+                        ),
+                ],
+              ),
+            ),
+            showArrow
+                ? Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: primary
+                        ? Theme.of(context).colorScheme.background
+                        : Theme.of(context).colorScheme.secondary,
+                    size: 20.0,
+                  )
+                : const SizedBox(),
+          ],
+        ),
       ),
     );
   }
