eeef32340d0d05279ab5c4d636632ef68ee7002c
Jakob <business@jakkoble.de>
2023-07-03 22:21:54 +0200

    added lesson model

---
+++ lib/models/lesson.dart
@@ -0,0 +1,19 @@
+class Lesson {
+  final String id;
+  final String title;
+  final String teacher;
+  final String room;
+  final String startTime;
+  final String endTime;
+  final String day;
+
+  const Lesson({
+    required this.id,
+    required this.title,
+    required this.teacher,
+    required this.room,
+    required this.startTime,
+    required this.endTime,
+    required this.day,
+  });
+}
