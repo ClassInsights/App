e347546321a52822207ee968bcf142fb08ae6b9c
Jakob <business@jakkoble.de>
2023-07-03 18:29:49 +0200

    implemented new content

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,19 +1,38 @@
 import 'package:classinsights/widgets/header.dart';
+import 'package:classinsights/widgets/progress_bar.dart';
 import 'package:classinsights/widgets/widget_container.dart';
 import 'package:flutter/material.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
+  final double minutes = 30.0;
+  final double baseMinutes = 50.0;
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Header(title: "Willkommen, Jakob."),
+        const Header(
+          title: "Willkommen, Jakob.",
+        ),
         Column(
           children: [
             WidgetContainer(
-              child: Text("Hello World!"),
+              label: "Aktuelle Stunde",
+              title: "NTMA", // TODO: Replace with actual class
+              child: ProgressBar(
+                title:
+                    'noch ${(baseMinutes - minutes).toStringAsFixed(0)} Minuten',
+                progress: minutes,
+                baseValue: baseMinutes,
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            const WidgetContainer(
+              title: "Deine Klassen",
             ),
           ],
         ),
