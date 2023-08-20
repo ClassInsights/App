65506f04c0fe9c7857efe2356ace60c51fcb3f67
Jakob <business@jakkoble.de>
2023-08-20 19:07:42 +0200

    added new widgets

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:classinsights/widgets/header.dart';
 import 'package:classinsights/widgets/progress_bar.dart';
+import 'package:classinsights/widgets/shortcuts/app_shortcut.dart';
 import 'package:classinsights/widgets/widget_container.dart';
 import 'package:flutter/material.dart';
 
@@ -11,28 +12,51 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const defaultPadding = 15.0;
+
     return Column(
       children: [
         const Header(
           title: "Willkommen, Jakob.",
         ),
-        Column(
-          children: [
-            WidgetContainer(
-              label: "Aktuelle Stunde",
-              title: "NTMA", // TODO: Replace with actual class
-              child: ProgressBar(
-                title:
-                    'noch ${(baseMinutes - minutes).toStringAsFixed(0)} Minuten',
-                progress: minutes,
-                baseValue: baseMinutes,
+        LayoutBuilder(builder: (context, constraints) {
+          return Column(
+            children: [
+              WidgetContainer(
+                label: "Aktuelle Stunde",
+                title: "NTMA", // TODO: Replace with actual class
+                child: ProgressBar(
+                  title: 'noch ${(baseMinutes - minutes).toStringAsFixed(0)} Minuten',
+                  progress: minutes,
+                  baseValue: baseMinutes,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-          ],
-        ),
+              const SizedBox(
+                height: defaultPadding,
+              ),
+              Row(
+                children: [
+                  WidgetContainer(
+                    label: "Top",
+                    title: "Hop",
+                    width: constraints.maxWidth / 2 - defaultPadding / 2,
+                  ),
+                  const SizedBox(
+                    width: defaultPadding,
+                  ),
+                  AppShortcut(constraints.maxWidth / 2 - defaultPadding / 2),
+                ],
+              ),
+              const SizedBox(
+                height: defaultPadding,
+              ),
+              const WidgetContainer(
+                label: "Aktive Computer",
+                title: "121",
+              )
+            ],
+          );
+        }),
       ],
     );
   }
