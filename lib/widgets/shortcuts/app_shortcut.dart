866ba3f8415ed04628a77c3a1d9a2680ca4cd0bf
Jakob <business@jakkoble.de>
2023-08-20 18:04:58 +0200

    added app_shortcut widget

---
+++ lib/widgets/shortcuts/app_shortcut.dart
@@ -0,0 +1,24 @@
+import 'package:classinsights/providers/screen_provider.dart';
+import 'package:classinsights/widgets/widget_container.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AppShortcut extends ConsumerWidget {
+  final double width;
+  const AppShortcut(this.width, {super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    openRooms() {
+      ref.read(screenProvider.notifier).setScreen(Screen.rooms);
+    }
+
+    return WidgetContainer(
+      title: "16",
+      label: "Räume",
+      width: width,
+      showArrow: true,
+      onTab: openRooms,
+    );
+  }
+}
