19178ce6c2495816925cad13e4698e7fd15d63e8
Jakob <business@jakkoble.de>
2023-08-22 18:38:42 +0200

    added room model

---
+++ lib/models/room.dart
@@ -0,0 +1,11 @@
+class Room {
+  final String id;
+  final String name;
+  final double deviceCount;
+
+  const Room({
+    required this.id,
+    required this.name,
+    required this.deviceCount,
+  });
+}
