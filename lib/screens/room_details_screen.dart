bf8a8c8df2de9d2e0222259f863d091a6a222bd0
Jakob <business@jakkoble.de>
2023-08-22 21:06:37 +0200

    add custom app bar for detail screens

--- lib/screens/room_details_screen.dart
+++ lib/screens/room_details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:classinsights/widgets/detail_appbar.dart';
 import 'package:classinsights/widgets/lesson.dart';
 import 'package:flutter/material.dart';
 
@@ -12,36 +13,23 @@ class RoomDetailScreen extends StatelessWidget {
       body: Container(
         width: double.infinity,
         padding: const EdgeInsets.symmetric(horizontal: 30.0),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20.0),
-          child: CustomScrollView(
-            physics: const BouncingScrollPhysics(),
-            slivers: [
-              SliverAppBar(
-                leading: IconButton(
-                  onPressed: () => onBack(),
-                  icon: const Icon(Icons.arrow_back_ios_rounded),
+        child: Column(
+          children: [
+            DetailAppbar(
+              title: "OG2-DV3",
+              onBack: onBack,
+            ),
+            const SingleChildScrollView(
+              child: Column(children: [
+                SizedBox(height: 20.0),
+                LessonWidget(
+                  title: "MAM",
+                  startTime: "2023-06-22T09:30:00.000Z",
+                  endTime: "2023-06-22T10:20:00.000Z",
                 ),
-                leadingWidth: 40.0,
-                pinned: true,
-                title: Text(
-                  "OG2-DV3",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                backgroundColor: Theme.of(context).colorScheme.background,
-              ),
-              const SliverToBoxAdapter(
-                child: Column(children: [
-                  SizedBox(height: 20.0),
-                  LessonWidget(
-                    title: "MAM",
-                    startTime: "2023-06-22T09:30:00.000Z",
-                    endTime: "2023-06-22T10:20:00.000Z",
-                  ),
-                ]),
-              )
-            ],
-          ),
+              ]),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/detail_appbar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DetailAppbar extends StatelessWidget {
+  final String title;
+  final Function onBack;
+  const DetailAppbar({required this.title, required this.onBack, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top + 20.0, bottom: 40.0),
+      padding: const EdgeInsets.only(top: 10),
+      child: Row(
+        children: [
+          IconButton(
+            padding: EdgeInsets.zero,
+            constraints: const BoxConstraints(),
+            onPressed: () => Navigator.of(context).pop(),
+            icon: const Icon(Icons.arrow_back_ios_rounded),
+          ),
+          const SizedBox(width: 20.0),
+          Text(
+            "OG2-DV3",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ],
+      ),
+    );
+  }
+}
