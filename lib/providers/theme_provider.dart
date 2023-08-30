58dd56b4362866e88f2d4dfeeee83d40b7ec826c
Jakob <business@jakkoble.de>
2023-08-30 22:41:16 +0200

    updated theme provider

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,14 +1,26 @@
+import 'package:classinsights/providers/localstore_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>(
-  (_) => ThemeNotifier(),
-);
+final themeProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>((ref) => ThemeNotifier(ref));
 
 class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.light);
+  final StateNotifierProviderRef ref;
+  ThemeNotifier(this.ref) : super(ThemeMode.light);
 
   ThemeMode get theme => state;
 
-  void switchTheme() => {state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light};
+  void refreshTheme(Brightness brightness) async {
+    final themeData = await ref.read(localstoreProvider.notifier).item("theme");
+    if (themeData != null) {
+      state = themeData.value == "light" ? ThemeMode.light : ThemeMode.dark;
+    } else {
+      state = brightness == Brightness.light ? ThemeMode.light : ThemeMode.dark;
+    }
+  }
+
+  void switchTheme() {
+    ref.read(localstoreProvider.notifier).setItem("theme", state == ThemeMode.light ? "dark" : "light");
+    state = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+  }
 }
