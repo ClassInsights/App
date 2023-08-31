391d3125ef9138998d0ed6d759bb4f231e4df586
Jakob <business@jakkoble.de>
2023-08-31 17:30:14 +0200

    added widget content

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,8 +1,11 @@
+import 'package:classinsights/providers/localstore_provider.dart';
 import 'package:classinsights/providers/theme_provider.dart';
+import 'package:classinsights/widgets/container_content.dart';
 import 'package:classinsights/widgets/header.dart';
 import 'package:classinsights/widgets/widget_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({super.key});
@@ -10,7 +13,19 @@ class ProfileScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var darkMode = Theme.of(context).brightness == Brightness.dark;
-    switchTheme() => ref.read(themeProvider.notifier).switchTheme();
+
+    void switchTheme() => ref.read(themeProvider.notifier).switchTheme();
+
+    void openGithub() async {
+      if (!await launchUrl(Uri.parse("https://github.com/ClassInsights/App"))) {
+        throw Exception("Could not launch Repository URL");
+      }
+    }
+
+    void resetTheme() => {
+          ref.read(localstoreProvider.notifier).clear(),
+          ref.read(themeProvider.notifier).refreshTheme(MediaQuery.of(context).platformBrightness),
+        };
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -26,13 +41,68 @@ class ProfileScreen extends ConsumerWidget {
         const Text("4KK (SIEM)"),
         const SizedBox(height: 10.0),
         const Text("26 Stunden/Woche"),
-        const SizedBox(height: 20.0),
-        WidgetContainer(
+        const SizedBox(height: 40.0),
+        ContainerWithContent(
           label: darkMode ? "Aktiviert" : "Deaktiviert",
           title: "Dunkler Modus",
           onTab: switchTheme,
           primary: true,
-        )
+        ),
+        const SizedBox(height: 15.0),
+        WidgetContainer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Adresse",
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              const SizedBox(height: 10.0),
+              const Text("HAK/HAS/HLW Landeck"),
+              const SizedBox(height: 10.0),
+              const Text("Kreuzgasse 9 a"),
+              const SizedBox(height: 10.0),
+              const Text("6500 Landeck"),
+              const SizedBox(height: 10.0),
+              const Text("Österreich"),
+            ],
+          ),
+        ),
+        const SizedBox(height: 15.0),
+        WidgetContainer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Details",
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              const SizedBox(height: 10.0),
+              const Text("HAK/HAS/HLW Landeck"),
+              const SizedBox(height: 10.0),
+              const Text("App Version: 1.0.3"),
+              const SizedBox(height: 10.0),
+              const Text("© 2023 HAK/HAS/HLW Landeck"),
+              const SizedBox(height: 10.0),
+              GestureDetector(
+                onTap: openGithub,
+                child: Text(
+                  "Source Code",
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 15.0),
+        ContainerWithContent(
+          label: "Zurücksetzen",
+          title: "Lokale Daten löschen",
+          onTab: resetTheme,
+          primary: true,
+        ),
       ],
     );
   }
