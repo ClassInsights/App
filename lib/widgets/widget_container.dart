77e93e1ded2f0196925062d80bd4a7e799f6aecd
Jakob <business@jakkoble.de>
2023-09-06 10:59:02 +0200

    adjusted shadows

--- lib/widgets/widget_container.dart
+++ lib/widgets/widget_container.dart
@@ -1,6 +1,8 @@
+import 'package:classinsights/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class WidgetContainer extends StatelessWidget {
+class WidgetContainer extends ConsumerWidget {
   final Widget child;
   final double width;
   final bool primary;
@@ -13,8 +15,9 @@ class WidgetContainer extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final backgroundColor = primary ? Theme.of(context).colorScheme.primaryContainer : Theme.of(context).colorScheme.secondaryContainer;
+  Widget build(BuildContext context, WidgetRef ref) {
+    var darkMode = ref.watch(themeProvider.notifier).theme == ThemeMode.dark;
+
     return Container(
       width: width,
       padding: const EdgeInsets.symmetric(
@@ -23,10 +26,11 @@ class WidgetContainer extends StatelessWidget {
       ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8.0),
-        color: backgroundColor,
+        color: primary ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.secondary,
         boxShadow: [
           BoxShadow(
-            color: backgroundColor.withOpacity(.5),
+            color: (primary ? Theme.of(context).colorScheme.primaryContainer : Theme.of(context).colorScheme.secondaryContainer)
+                .withOpacity(darkMode ? .1 : .1),
             blurRadius: 5.0,
             offset: const Offset(2.0, 4.0),
           ),
