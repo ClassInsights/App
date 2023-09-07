a858b5b8511eccd8adcade01a09e669dcd596dfb
Jakob <business@jakkoble.de>
2023-09-07 15:45:51 +0200

    added room provider

---
+++ lib/providers/room_provider.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:classinsights/models/room.dart';
+import 'package:classinsights/providers/auth_provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import "package:http/http.dart" as http;
+
+final roomProvider = StateNotifierProvider<RoomNotifier, List<Room>>((ref) => RoomNotifier(ref));
+
+class RoomNotifier extends StateNotifier<List<Room>> {
+  final StateNotifierProviderRef ref;
+  RoomNotifier(this.ref) : super([]);
+
+  List<Room> get rooms => state;
+
+  Future<List<Room>> fetchRooms() async {
+    if (state.isNotEmpty) return state;
+    final client = http.Client();
+    final response = await client.get(
+      Uri.parse("${dotenv.env['API_URL'] ?? ""}/rooms"),
+      headers: {
+        "Authorization": "Bearer ${ref.read(authProvider).creds.accessToken}",
+      },
+    );
+    if (response.statusCode != 200) return [];
+
+    final data = jsonDecode(response.body);
+    final rooms = data.map<Room>((room) {
+      return Room(
+        id: room["roomId"],
+        name: room["longName"].toString().length >= 30 ? room["name"] : room["longName"],
+        deviceCount: room["deviceCount"],
+      );
+    }).toList();
+
+    if (rooms.isNotEmpty) state = rooms;
+    return rooms;
+  }
+}
