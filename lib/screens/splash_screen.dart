094e99c9502cddb6f01776a0feac36ae39677744
Jakob <business@jakkoble.de>
2023-09-07 12:54:43 +0200

    await provider initialization

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,31 +1,57 @@
 import 'package:classinsights/providers/auth_provider.dart';
+import 'package:classinsights/providers/room_provider.dart';
+import 'package:classinsights/providers/version_provider.dart';
 import 'package:classinsights/screens/login_screen.dart';
 import 'package:classinsights/screens/tabs.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplashScreen extends ConsumerWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    ref.read(authProvider.notifier).verifyLogin().then(
-          (valid) => valid
-              ? Navigator.of(context).push(
-                  PageRouteBuilder(
-                    pageBuilder: (context, firstAnimation, secondAnimation) => const TabsScreen(),
-                    transitionDuration: Duration.zero,
-                    reverseTransitionDuration: Duration.zero,
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  bool alreadyInitialized = false;
+
+  Future<void> initProviders() async {
+    await ref.read(authProvider.notifier).reload();
+    await ref.read(versionProvider.notifier).fetchVersion();
+    var validLogin = ref.read(authProvider).creds.accessToken.isNotEmpty;
+    if (validLogin) {
+      await ref.read(roomProvider.notifier).fetchRooms();
+    }
+  }
+
+  @override
+  void initState() {
+    if (ref.read(authProvider).creds.accessToken.isNotEmpty) {
+      setState(() => alreadyInitialized = true);
+      return;
+    }
+    initProviders().then((_) => setState(() => alreadyInitialized = true));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (alreadyInitialized) {
+      ref.read(authProvider.notifier).verifyLogin().then(
+            (valid) => valid
+                ? Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const TabsScreen()),
+                  )
+                : Navigator.of(context).push(
+                    PageRouteBuilder(
+                      pageBuilder: (context, firstAnimation, secondAnimation) => const LoginScreen(),
+                      transitionDuration: Duration.zero,
+                      reverseTransitionDuration: Duration.zero,
+                    ),
                   ),
-                )
-              : Navigator.of(context).push(
-                  PageRouteBuilder(
-                    pageBuilder: (context, firstAnimation, secondAnimation) => const LoginScreen(),
-                    transitionDuration: Duration.zero,
-                    reverseTransitionDuration: Duration.zero,
-                  ),
-                ),
-        );
+          );
+    }
 
     return Scaffold(
       body: SizedBox(
