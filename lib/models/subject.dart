2ddc0d4362cac591b2a14a502ea7372e0de2a270
Jakob <business@jakkoble.de>
2023-09-09 11:15:37 +0200

    added subject model

---
+++ lib/models/subject.dart
@@ -0,0 +1,11 @@
+class Subject {
+  final int id;
+  final String name;
+  final String longName;
+
+  const Subject({
+    required this.id,
+    required this.name,
+    required this.longName,
+  });
+}
