49efc16332c1011ffa8765231fda96e87d0b664a
Jakob <business@jakkoble.de>
2023-09-09 16:22:31 +0200

    added profile screen content

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,73 +1,60 @@
+import 'package:classinsights/main.dart';
+import 'package:classinsights/providers/auth_provider.dart';
+import 'package:classinsights/providers/lesson_provider.dart';
 import 'package:classinsights/providers/localstore_provider.dart';
 import 'package:classinsights/providers/theme_provider.dart';
+import 'package:classinsights/providers/version_provider.dart';
+import 'package:classinsights/screens/login_screen.dart';
 import 'package:classinsights/widgets/container_content.dart';
 import 'package:classinsights/widgets/header.dart';
 import 'package:classinsights/widgets/widget_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class ProfileScreen extends ConsumerStatefulWidget {
+class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({super.key});
 
   @override
-  ConsumerState<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends ConsumerState<ProfileScreen> {
-  var version = "Unknown";
-
-  Future<String> fetchVersion() async {
-    var info = await PackageInfo.fromPlatform();
-    return info.version;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchVersion().then((data) => setState(() => version = data));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var darkMode = Theme.of(context).brightness == Brightness.dark;
-
-    void switchTheme() => ref.read(themeProvider.notifier).switchTheme();
-
+  Widget build(BuildContext context, WidgetRef ref) {
     void openGithub() async {
       if (!await launchUrl(Uri.parse("https://github.com/ClassInsights/App"))) {
         throw Exception("Could not launch Repository URL");
       }
     }
 
-    void resetData() => {
-          ref.read(localstoreProvider.notifier).clear(),
-          ref.read(themeProvider.notifier).refreshTheme(MediaQuery.of(context).platformBrightness),
-        };
+    void resetData() {
+      ref.read(localstoreProvider.notifier).clear();
+      ref.read(themeProvider.notifier).refreshTheme(brightness: MediaQuery.of(context).platformBrightness);
+      ref.read(authProvider.notifier).logout();
+    }
 
+    final authData = ref.read(authProvider).data;
+    final todaysLessons = ref.read(lessonProvider.notifier).getLessonsForDay(DateTime.now());
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const Header("Konto"),
         Text(
-          "Jakob Wassertheurer",
+          authData.name,
           style: Theme.of(context).textTheme.titleSmall,
         ),
-        const SizedBox(height: 15.0),
-        const Text("Schüler"),
-        const SizedBox(height: 10.0),
-        const Text("4KK (SIEM)"),
-        const SizedBox(height: 10.0),
-        const Text("26 Stunden/Woche"),
+        const SizedBox(height: App.defaultPadding),
+        authData.role != null ? Text(ref.read(authProvider.notifier).translateRole(authData.role!)) : const SizedBox.shrink(),
+        const SizedBox(height: App.smallPadding),
+        authData.schoolClass != null
+            ? Text("${authData.schoolClass!.name} ${authData.schoolClass?.headTeacher != null ? "(${authData.schoolClass!.headTeacher})" : ""}")
+            : const SizedBox.shrink(),
+        const SizedBox(height: App.smallPadding),
+        todaysLessons.isEmpty ? const Text("Schulfreier Tag") : Text("${todaysLessons.length} Stunden heute"),
         const SizedBox(height: 40.0),
         ContainerWithContent(
-          label: darkMode ? "Aktiviert" : "Deaktiviert",
-          title: "Dunkler Modus",
-          onTab: switchTheme,
+          label: "Dunkler Modus",
+          title: ref.read(themeProvider) == ThemeMode.dark ? "Aktiviert" : "Deaktiviert",
+          onTab: () => ref.read(themeProvider.notifier).switchTheme(),
           primary: true,
         ),
-        const SizedBox(height: 15.0),
+        const SizedBox(height: App.defaultPadding),
         WidgetContainer(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -76,18 +63,18 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                 "Adresse",
                 style: Theme.of(context).textTheme.titleSmall,
               ),
-              const SizedBox(height: 10.0),
+              const SizedBox(height: App.defaultPadding),
               const Text("HAK/HAS/HLW Landeck"),
-              const SizedBox(height: 10.0),
+              const SizedBox(height: App.smallPadding),
               const Text("Kreuzgasse 9 a"),
-              const SizedBox(height: 10.0),
+              const SizedBox(height: App.smallPadding),
               const Text("6500 Landeck"),
-              const SizedBox(height: 10.0),
+              const SizedBox(height: App.smallPadding),
               const Text("Österreich"),
             ],
           ),
         ),
-        const SizedBox(height: 15.0),
+        const SizedBox(height: App.defaultPadding),
         WidgetContainer(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -96,30 +83,39 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
                 "Details",
                 style: Theme.of(context).textTheme.titleSmall,
               ),
-              const SizedBox(height: 10.0),
-              const Text("HAK/HAS/HLW Landeck"),
-              const SizedBox(height: 10.0),
-              Text("App Version: $version"),
-              const SizedBox(height: 10.0),
+              const SizedBox(height: App.defaultPadding),
+              const Text("ClassInsights App"),
+              const SizedBox(height: App.smallPadding),
+              Text("App Version: ${ref.read(versionProvider)}"),
+              const SizedBox(height: App.smallPadding),
               Text("© ${DateTime.now().year} HAK/HAS/HLW Landeck"),
-              const SizedBox(height: 10.0),
+              const SizedBox(height: App.smallPadding),
               GestureDetector(
                 onTap: openGithub,
                 child: Text(
                   "Quellcode",
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
                 ),
               ),
             ],
           ),
         ),
-        const SizedBox(height: 15.0),
+        const SizedBox(height: App.defaultPadding),
         ContainerWithContent(
           label: "Zurücksetzen",
           title: "Lokale Daten löschen",
-          onTab: resetData,
+          onTab: () {
+            resetData();
+            Navigator.of(context).pushReplacement(
+              PageRouteBuilder(
+                pageBuilder: (context, firstAnimation, secondAnimation) => const LoginScreen(),
+                transitionDuration: Duration.zero,
+                reverseTransitionDuration: Duration.zero,
+              ),
+            );
+          },
           primary: true,
         ),
       ],
