bba8a214bb07d7ead60070c1ad59616b02d4708b
Jakob <business@jakkoble.de>
2023-09-10 21:57:19 +0200

    added computer overview to room details screen

--- lib/screens/room_details_screen.dart
+++ lib/screens/room_details_screen.dart
@@ -1,36 +1,114 @@
+import 'package:classinsights/main.dart';
+import 'package:classinsights/providers/lesson_provider.dart';
+import 'package:classinsights/providers/room_provider.dart';
+import 'package:classinsights/widgets/computer_list.dart';
+import 'package:classinsights/widgets/container_content.dart';
 import 'package:classinsights/widgets/detail_appbar.dart';
-import 'package:classinsights/widgets/lesson.dart';
+import 'package:classinsights/widgets/lesson_widget.dart';
+import 'package:classinsights/widgets/widget_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RoomDetailScreen extends StatelessWidget {
-  final String roomID;
+class RoomDetailScreen extends ConsumerWidget {
+  final int roomID;
   const RoomDetailScreen(this.roomID, {super.key});
 
   @override
-  Widget build(BuildContext context) {
-    onBack() => Navigator.of(context).pop();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final room = ref.read(roomProvider.notifier).getRoomById(roomID);
+    final lesson = ref.read(lessonProvider.notifier).getLessonByDate(DateTime.now());
+
     return Scaffold(
       body: Container(
         width: double.infinity,
         padding: const EdgeInsets.symmetric(horizontal: 30.0),
-        child: Column(
-          children: [
-            DetailAppbar(
-              title: "OG2-DV3",
-              onBack: onBack,
-            ),
-            const SingleChildScrollView(
-              child: Column(children: [
-                SizedBox(height: 20.0),
-                LessonWidget(
-                  title: "MAM",
-                  startTime: "2023-06-22T09:30:00.000Z",
-                  endTime: "2023-06-22T10:20:00.000Z",
+        child: room != null
+            ? Column(
+                children: [
+                  DetailAppbar(
+                    title: room.name,
+                    onBack: () => Navigator.of(context).pop(),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          lesson != null
+                              ? LessonWidget(lesson: lesson)
+                              : const ContainerWithContent(
+                                  label: "Aktuelle Stunde",
+                                  title: "Hier ist gerade keine Stunde",
+                                ),
+                          const SizedBox(height: App.defaultPadding),
+                          WidgetContainer(
+                            primary: true,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Raumdaten",
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context).textTheme.titleSmall!.fontSize,
+                                    color: Theme.of(context).colorScheme.onPrimary,
+                                  ),
+                                ),
+                                const SizedBox(height: App.defaultPadding),
+                                Text(
+                                  "aktuell 21°C",
+                                  style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+                                ),
+                                const SizedBox(height: App.smallPadding),
+                                Text(
+                                  "hervorragende Luftqualität",
+                                  style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+                                ),
+                                const SizedBox(height: App.smallPadding),
+                                Text(
+                                  "36% Luftfeuchtigkeit",
+                                  style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 50.0),
+                          Text(
+                            "Registrierte Computer",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          const SizedBox(height: App.defaultPadding),
+                          ComputerList(room.id)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Klasse nicht gefunden", style: Theme.of(context).textTheme.titleMedium),
+                    const SizedBox(height: 20.0),
+                    const Text(
+                      "Bitte versuche es erneut. Wenn das Problem weiterhin besteht, kontaktiere uns.",
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20.0),
+                    ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      child: Text(
+                        "Zurück",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                          fontSize: Theme.of(context).textTheme.bodyLarge!.fontSize,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ]),
-            ),
-          ],
-        ),
+              ),
       ),
     );
   }
---
+++ lib/widgets/computer_list.dart
@@ -0,0 +1,56 @@
+import 'package:classinsights/main.dart';
+import 'package:classinsights/providers/computer_provider.dart';
+import 'package:classinsights/screens/computer_detail_screen.dart';
+import 'package:classinsights/widgets/container_content.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ComputerList extends ConsumerStatefulWidget {
+  final int classId;
+  const ComputerList(this.classId, {super.key});
+
+  @override
+  ConsumerState<ComputerList> createState() => _ComputerListState();
+}
+
+class _ComputerListState extends ConsumerState<ComputerList> {
+  var loading = true;
+
+  @override
+  void initState() {
+    ref.read(computerProvider.notifier).fetchComputers(widget.classId).then((_) => setState(() => loading = false));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (loading) return const Center(child: CircularProgressIndicator());
+    var computers = ref.read(computerProvider);
+    return computers.isNotEmpty
+        ? ListView.separated(
+            padding: const EdgeInsets.all(0.0),
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: computers.length,
+            shrinkWrap: true,
+            separatorBuilder: (context, _) => const SizedBox(
+              height: App.defaultPadding,
+            ),
+            itemBuilder: (context, index) {
+              final computer = computers[index];
+              return ContainerWithContent(
+                label: "Computer",
+                title: computer.name,
+                showArrow: true,
+                onTab: () => Navigator.of(context).push(
+                  PageRouteBuilder(
+                    pageBuilder: (context, firstAnimation, secondAnimation) => ComputerDetailScreen(computer),
+                    transitionDuration: Duration.zero,
+                    reverseTransitionDuration: Duration.zero,
+                  ),
+                ),
+              );
+            },
+          )
+        : const Text("Keine Computer registriert");
+  }
+}
