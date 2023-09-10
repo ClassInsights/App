f0bb13889e9537c95e1c873d4e2c0d4ce664dee0
Jakob <business@jakkoble.de>
2023-09-10 21:57:26 +0200

    added computer detail screen

---
+++ lib/screens/computer_detail_screen.dart
@@ -0,0 +1,62 @@
+import 'package:classinsights/main.dart';
+import 'package:classinsights/models/computer.dart';
+import 'package:classinsights/widgets/detail_appbar.dart';
+import 'package:classinsights/widgets/widget_container.dart';
+import 'package:flutter/material.dart';
+
+class ComputerDetailScreen extends StatefulWidget {
+  final Computer computer;
+  const ComputerDetailScreen(this.computer, {super.key});
+
+  @override
+  State<ComputerDetailScreen> createState() => ComputerDetailScreenState();
+}
+
+class ComputerDetailScreenState extends State<ComputerDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          children: [
+            DetailAppbar(
+              title: widget.computer.name,
+              onBack: () => Navigator.of(context).pop(),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      WidgetContainer(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Computerdaten",
+                              style: Theme.of(context).textTheme.titleSmall,
+                            ),
+                            const SizedBox(height: App.defaultPadding),
+                            Text("Letzter Nutzer: ${widget.computer.lastUser}"),
+                            const SizedBox(height: App.smallPadding),
+                            Text(
+                                "Zuletzt verwendet: ${widget.computer.lastSeen.day}-${widget.computer.lastSeen.month}-${widget.computer.lastSeen.year}"),
+                            const SizedBox(height: App.smallPadding),
+                            Text("IP: ${widget.computer.ipAddress}"),
+                            const SizedBox(height: App.smallPadding),
+                            Text("MAC: ${widget.computer.macAddress}"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
