f4fb841dff73a0ff5ff82cf333356a8ac87a6f05
Jakob <business@jakkoble.de>
2023-09-15 15:09:33 +0200

    implemented swipe navigation

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -15,55 +15,43 @@ class TabsScreen extends ConsumerStatefulWidget {
 }
 
 class _TabsScreenState extends ConsumerState<TabsScreen> {
-  var _currentIndex = 0;
-  Widget _currentScreen = const DashboardScreen();
+  int _currentIndex = 0;
 
-  final _screens = const [
-    DashboardScreen(key: Key("DashboardScreen")),
-    ClassesScreen(key: Key("ClassesScreen")),
-    ProfileScreen(key: Key("ProfileScreen")),
+  final _screens = [
+    const DashboardScreen(key: Key("dashboard_screen")),
+    const ClassesScreen(key: Key("classes_screen")),
+    const ProfileScreen(key: Key("profile_screen")),
   ];
 
   @override
   Widget build(BuildContext context) {
     final scrollController = ScrollController();
-    setScreen(int index) => setState(() {
-          _currentIndex = index;
-          ref.read(screenProvider.notifier).setScreen(Screen.values[index]);
-        });
+    var pageController = PageController(initialPage: 0);
+
+    onChangedScreen(int index) {
+      setState(() => _currentIndex = index);
+      ref.read(screenProvider.notifier).setScreen(Screen.values[index]);
+    }
+
+    onTabNavigation(int index) => setState(
+          () {
+            _currentIndex = index;
+            pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.easeInOut,
+            );
+          },
+        );
 
     ref.listen(screenProvider, (_, newScreen) {
       scrollController.jumpTo(0.0);
-      setScreen(Screen.values.indexOf(newScreen));
-      setState(() => _currentScreen = _screens[_currentIndex]);
+      pageController.animateToPage(
+        Screen.values.indexOf(newScreen),
+        duration: const Duration(milliseconds: 150),
+        curve: Curves.easeInOut,
+      );
     });
-
-    var bottomNavigationBar = BottomNavigationBar(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      elevation: 0,
-      iconSize: 30.0,
-      onTap: setScreen,
-      currentIndex: _currentIndex,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      selectedItemColor: Theme.of(context).colorScheme.primary,
-      unselectedItemColor:
-          ref.read(themeProvider) == ThemeMode.dark ? Theme.of(context).colorScheme.tertiary : Theme.of(context).colorScheme.secondary,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_rounded),
-          label: "Home",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.class_outlined),
-          label: "Classes",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person_outline_outlined),
-          label: "Profile",
-        ),
-      ],
-    );
 
     return Scaffold(
       body: Column(
@@ -74,25 +62,114 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
             index: _currentIndex,
           ),
           Expanded(
-            child: SingleChildScrollView(
-              controller: scrollController,
+            child: PageView(
               physics: const BouncingScrollPhysics(),
-              child: Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(top: 30.0),
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Column(
-                  children: [
-                    _currentScreen,
-                    const SizedBox(height: 50.0),
-                  ],
-                ),
-              ),
+              onPageChanged: onChangedScreen,
+              controller: pageController,
+              children: _screens
+                  .map(
+                    (screen) => SingleChildScrollView(
+                      controller: scrollController,
+                      physics: const BouncingScrollPhysics(),
+                      child: Container(
+                        width: double.infinity,
+                        margin: const EdgeInsets.only(top: 30.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        child: screen,
+                      ),
+                    ),
+                  )
+                  .toList(),
             ),
           ),
         ],
       ),
-      bottomNavigationBar: bottomNavigationBar,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        elevation: 0,
+        iconSize: 30.0,
+        onTap: onTabNavigation,
+        currentIndex: _currentIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor:
+            ref.read(themeProvider) == ThemeMode.dark ? Theme.of(context).colorScheme.tertiary : Theme.of(context).colorScheme.secondary,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.class_outlined),
+            label: "Classes",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_outlined),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
+
+    // return Scaffold(
+    //   body: Column(
+    //     children: [
+    //       CustomAppBar(
+    //         height: 50,
+    //         title: "HAK/HAS/HLW Landeck",
+    //         index: _currentIndex,
+    //       ),
+    //       Expanded(
+    //         child: SingleChildScrollView(
+    //           controller: scrollController,
+    //           physics: const BouncingScrollPhysics(),
+    //           child: Container(
+    //             width: double.infinity,
+    //             margin: const EdgeInsets.only(top: 30.0),
+    //             padding: const EdgeInsets.symmetric(horizontal: 30.0),
+    //             child: Expanded(
+    //               child: TabBarView(
+    //                 controller: ,
+    //                 physics: const BouncingScrollPhysics(),
+    //                 children: const [
+    //                   Center(child: Text("Dashboard")),
+    //                   Center(child: Text("Rooms")),
+    //                   Center(child: Text("Profile")),
+    //                 ],
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    //   bottomNavigationBar: BottomNavigationBar(
+    //     backgroundColor: Theme.of(context).colorScheme.background,
+    //     elevation: 0,
+    //     iconSize: 30.0,
+    //     onTap: onTabTapped,
+    //     currentIndex: _currentIndex,
+    //     showSelectedLabels: false,
+    //     showUnselectedLabels: false,
+    //     selectedItemColor: Theme.of(context).colorScheme.primary,
+    //     unselectedItemColor:
+    //         ref.read(themeProvider) == ThemeMode.dark ? Theme.of(context).colorScheme.tertiary : Theme.of(context).colorScheme.secondary,
+    //     items: const [
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.home_rounded),
+    //         label: "Home",
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.class_outlined),
+    //         label: "Classes",
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.person_outline_outlined),
+    //         label: "Profile",
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
