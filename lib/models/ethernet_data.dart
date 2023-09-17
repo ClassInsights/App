6ad9ed1a8f661b3252eaf369860bb2bef5cee8af
Jakob <business@jakkoble.de>
2023-09-17 17:54:39 +0200

    added new models

---
+++ lib/models/ethernet_data.dart
@@ -0,0 +1,9 @@
+class EthernetData {
+  final String uploadSpeed;
+  final String downloadSpeed;
+
+  const EthernetData({
+    required this.uploadSpeed,
+    required this.downloadSpeed,
+  });
+}
