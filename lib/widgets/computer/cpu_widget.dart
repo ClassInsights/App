a8c22b47081e84e5247d2b979d5cdc433b1a16e5
Jakob <business@jakkoble.de>
2023-09-18 19:21:07 +0200

    added cpu widgets

---
+++ lib/widgets/computer/cpu_widget.dart
@@ -0,0 +1,33 @@
+import 'package:classinsights/main.dart';
+import 'package:classinsights/models/computer_heartbeat.dart';
+import 'package:classinsights/widgets/charts/cpu_chart.dart';
+import 'package:classinsights/widgets/container/widget_container.dart';
+import 'package:flutter/material.dart';
+
+class CPUWidget extends StatelessWidget {
+  final ComputerHeartbeat data;
+  const CPUWidget(this.data, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WidgetContainer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("CPU Auslastung", style: Theme.of(context).textTheme.titleSmall),
+          const SizedBox(height: App.defaultPadding),
+          GridView.count(
+            crossAxisCount: 2,
+            padding: const EdgeInsets.all(0),
+            childAspectRatio: 10 / 6,
+            mainAxisSpacing: App.smallPadding,
+            crossAxisSpacing: App.smallPadding,
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            children: data.cpuUsage.map((cpuUsage) => const CPUChart()).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
