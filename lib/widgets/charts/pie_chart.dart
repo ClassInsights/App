93941107d153f27328fd01f8d8e2cfb8b50c3554
Jakob <business@jakkoble.de>
2023-12-06 21:14:26 +0100

    uploaded latest

--- lib/widgets/charts/pie_chart.dart
+++ lib/widgets/charts/pie_chart.dart
@@ -1,4 +1,3 @@
-import 'package:classinsights/main.dart';
 import 'package:classinsights/widgets/charts/chart_container.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
